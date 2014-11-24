c2866c2f788475cdba49c26b30710cab96b56ad3
Carlos Crespo <carlos.crespog@gmail.com>
2014-11-24 19:07:00 +0100

    Parsing of YAML almost finished, had to go home

--- PyGCS.py
+++ PyGCS.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 # Cool stuff
+import wxversion
+print wxversion.getInstalled()
 import wx
 import socket
 import yaml
+import wx.lib.scrolledpanel as scrolled
+from random import randrange
 
+print wx.__version__
 class MyFrame(wx.Frame):
     
     def __init__(self, parent, id, title):
@@ -15,23 +20,29 @@ class MyFrame(wx.Frame):
         wx.Frame.__init__(self, parent, id, title, pos=(0,0), size=wx.DisplaySize())
 
         splitter = wx.SplitterWindow(self, -1)
-        commandsPanel = wx.Panel(splitter, -1)
+        self.commandsPanel = wx.Panel(splitter, -1)
        
-        commandsPanel.SetBackgroundColour(wx.LIGHT_GREY)
-        consolePanel = wx.Panel(splitter, -1)
-        consolePanel.SetBackgroundColour(wx.WHITE)
-        splitter.SplitHorizontally(commandsPanel, consolePanel,wx.DisplaySize()[1]*2/3)
-        
-        lostButton=wx.Button(commandsPanel,1,'LOST', (-1,-1))
-        lomaButton=wx.Button(commandsPanel,2,'LOMA', (-1,-1))
-
-        self.Bind(wx.EVT_BUTTON,self.sendCommand,id=1)
-        self.Bind(wx.EVT_BUTTON,self.sendCommand,id=2)  
+        self.commandsPanel.SetBackgroundColour(wx.LIGHT_GREY)
+        self.consolePanel = Terminal(splitter)
+        # consolePanel.SetBackgroundColour(wx.WHITE)
+        splitter.SplitHorizontally(self.commandsPanel, self.consolePanel,wx.DisplaySize()[1]*4/5)
 
         self.vbox = wx.BoxSizer(wx.VERTICAL)
-        self.vbox.Add(lostButton,0,wx.ALIGN_LEFT|wx.ALL,5)
-        self.vbox.Add(lomaButton,0,wx.ALIGN_LEFT|wx.ALL,5)
-        commandsPanel.SetSizer(self.vbox)
+        
+        self.config = yaml.load(open("config.yaml"))
+        self.visualMatrix = []
+        self.populateGUI()
+
+        # lostButton=wx.Button(self.commandsPanel,1,'LOST', (-1,-1))
+        # lomaButton=wx.Button(self.commandsPanel,2,'LOMA', (-1,-1))
+
+        # self.Bind(wx.EVT_BUTTON,self.sendCommand,id=1)
+        # self.Bind(wx.EVT_BUTTON,self.sendCommand,id=2)  
+
+        
+        # self.vbox.Add(lostButton,0,wx.ALIGN_LEFT|wx.ALL,5)
+        # self.vbox.Add(lomaButton,0,wx.ALIGN_LEFT|wx.ALL,5)
+        self.commandsPanel.SetSizer(self.vbox)
 
         self.upLink=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.downLink=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -41,15 +52,73 @@ class MyFrame(wx.Frame):
 
         self.Centre()
 
-        self.config = yaml.load(open("config.yaml"))
-        print self.config
+        
+        # print self.config
 
 
     def sendCommand (self,event):
         print event.GetId()
-        self.upLink.send("1 CSPFSTC TR PFS ROSO 1\n")
+        command="1 CSPFSTC TR PFS ROSO 1\n"
+        self.upLink.send(command)
         data=self.downLink.recv(self.BUFFER_SIZE)
-        print data      
+        self.consolePanel.append(command+" -> "+data)
+
+    def populateGUI(self):
+        # print config
+        for i in self.config["telecommand"]:
+            hbox=wx.BoxSizer(wx.HORIZONTAL)
+            button=wx.Button(self.commandsPanel,i["id"],i["code"],(-1,-1))
+            hbox.Add(button,-1,wx.ALL,0)
+            paramList=i["params"]
+            if (paramList==0):
+                pass
+            else:
+                for idx,param in enumerate(paramList.split(',')):
+                    paramText=wx.StaticText(self.commandsPanel,-1,param,(100,20))
+                    ranges=i["ranges"].split(',')
+                    minLim=float(ranges[idx*2])
+                    maxLim=float(ranges[(idx*2)+1])
+                    midValue=((maxLim+minLim)/2.)
+                    print param,minLim,midValue,maxLim
+                    if maxLim>1:
+                        paramSlider=wx.Slider(self.commandsPanel,-1,value=midValue,minValue=minLim,maxValue=maxLim,size=(200,20),style=wx.SL_HORIZONTAL|wx.SL_MIN_MAX_LABELS,name=param)
+                    # It's a decimal!:
+                    else:
+                        # paramSlider.SetTickFreq((minLim-maxLim)/10.)
+                        pass
+                    # 
+                    hbox.Add(paramText)
+                    hbox.Add(paramSlider)
+
+
+            self.vbox.Add(hbox,1,wx.ALL,0)
+
+
+class Terminal(scrolled.ScrolledPanel):
+
+    def __init__(self, parent):
+
+        scrolled.ScrolledPanel.__init__(self, parent, -1)
+        self.first=True
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        text  = "Welcome to pyGCS"
+
+        self.terminalBox = wx.TextCtrl(self,0,text,style=wx.TE_MULTILINE)
+        self.terminalBox.SetBackgroundColour((randrange(255),randrange(255),randrange(255)))
+        # self.SetBackgroundColour((255,255,0))
+        hbox.Add(self.terminalBox,1,wx.EXPAND|wx.TOP,5)
+        self.SetSizer(hbox)
+        # vbox.Add(hbox,1,wx.EXPAND,0)
+        # self.SetSizer(vbox)
+        self.SetAutoLayout(1) 
+        self.SetupScrolling()
+
+    def append(self,text):
+        if(self.first):
+            self.terminalBox.Clear()
+            self.first=False
+        self.terminalBox.AppendText(text)
 
 class MyApp(wx.App):
     def OnInit(self):
