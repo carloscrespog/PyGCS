636d961e15b34958d616724085828c765a48ea9c
Carlos Crespo <carlos.crespog@gmail.com>
2015-02-26 10:48:36 +0100

    RTCC included

--- PyGCS.py
+++ PyGCS.py
@@ -6,16 +6,23 @@ import wx
 import socket
 import yaml
 import wx.lib.scrolledpanel as scrolled
+import wx.lib.agw.floatspin as FS
 from random import randrange
-
-print wx.__version__
+import numpy as np
+import colorsys
 class MyFrame(wx.Frame):
     
     def __init__(self, parent, id, title):
-        LRM_IP='192.168.1.200'
-        LRM_TC_PORT=7131
-        LRM_TM_PORT=7133
+        self.parent=parent
+        self.LRM_IP='192.168.1.200'
+        self.LRM_TC_PORT=7131
+        self.LRM_TM_PORT=7133
+        self.LRM_TLM_PORT=7135
+        self.LRM_TLM_TM_PORT=7136
+        WINDOW_RATIO=4/5.
         self.BUFFER_SIZE = 1024
+        self.commandCount=1
+        self.TLM_OFFSET=20
 
         wx.Frame.__init__(self, parent, id, title, pos=(0,0), size=wx.DisplaySize())
 
@@ -24,51 +31,57 @@ class MyFrame(wx.Frame):
        
         self.commandsPanel.SetBackgroundColour(wx.LIGHT_GREY)
         self.consolePanel = Terminal(splitter)
-        # consolePanel.SetBackgroundColour(wx.WHITE)
-        splitter.SplitHorizontally(self.commandsPanel, self.consolePanel,wx.DisplaySize()[1]*4/5)
+
+        splitter.SplitHorizontally(self.commandsPanel, self.consolePanel,wx.DisplaySize()[1]*WINDOW_RATIO)
 
         self.vbox = wx.BoxSizer(wx.VERTICAL)
         
         self.config = yaml.load(open("config.yaml"))
         self.visualMatrix = []
-        self.populateGUI()
+        self.populateGUI(self.config["telecommand"])
+        self.populateGUI(self.config["telemanipulation"])
 
-        # lostButton=wx.Button(self.commandsPanel,1,'LOST', (-1,-1))
-        # lomaButton=wx.Button(self.commandsPanel,2,'LOMA', (-1,-1))
-
-        # self.Bind(wx.EVT_BUTTON,self.sendCommand,id=1)
-        # self.Bind(wx.EVT_BUTTON,self.sendCommand,id=2)  
-
-        
-        # self.vbox.Add(lostButton,0,wx.ALIGN_LEFT|wx.ALL,5)
-        # self.vbox.Add(lomaButton,0,wx.ALIGN_LEFT|wx.ALL,5)
         self.commandsPanel.SetSizer(self.vbox)
-
-        self.upLink=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.downLink=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-        self.upLink.connect((LRM_IP,LRM_TC_PORT))
-        self.downLink.connect((LRM_IP,LRM_TM_PORT))
-
+        self.connect()
+        
         self.Centre()
 
-        
-        # print self.config
 
+    def connect(self):
+        self.upLink=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.downLink=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.upLinkTLM=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.downLinkTLM=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-    def sendCommand (self,event):
-        print event.GetId()
-        command="1 CSPFSTC TR PFS ROSO 1\n"
-        self.upLink.send(command)
+        self.upLink.connect((self.LRM_IP,self.LRM_TC_PORT))
+        self.downLink.connect((self.LRM_IP,self.LRM_TM_PORT))
+        self.upLinkTLM.connect((self.LRM_IP,self.LRM_TLM_PORT))
+        self.downLinkTLM.connect((self.LRM_IP,self.LRM_TLM_TM_PORT))
+
+    def sendCommand (self,command):
+        commandLine=str(self.commandCount)+' CSPFSTC TR PFS '+command+'\n'
+        self.commandCount+=1
+        print commandLine
+        self.upLink.send(commandLine)
         data=self.downLink.recv(self.BUFFER_SIZE)
+        print data
         self.consolePanel.append(command+" -> "+data)
 
-    def populateGUI(self):
+    def sendCommandTLM (self,command):
+        commandLine=str(self.commandCount)+' CSVFSTC TR VFS TLM_LRM_TC '+command+'\n'
+        self.commandCount+=1
+        print commandLine
+        self.upLinkTLM.send(commandLine)
+        # data=self.downLink.recv(self.BUFFER_SIZE)
+        # print data
+        # self.consolePanel.append(command+" -> "+data)
+
+    def populateGUI(self,commands):
         # print config
-        for i in self.config["telecommand"]:
+        for i in commands:
             hbox=wx.BoxSizer(wx.HORIZONTAL)
             button=wx.Button(self.commandsPanel,i["id"],i["code"],(-1,-1))
-            hbox.Add(button,-1,wx.ALL,0)
+            hbox.Add(button,-1,wx.ALL,5)
             paramList=i["params"]
             if (paramList==0):
                 pass
@@ -80,18 +93,51 @@ class MyFrame(wx.Frame):
                     maxLim=float(ranges[(idx*2)+1])
                     midValue=((maxLim+minLim)/2.)
                     print param,minLim,midValue,maxLim
+                    paramFloatSpin=FS.FloatSpin(self.commandsPanel,id=100*i["id"]+idx,value=midValue,min_val=minLim,max_val=maxLim,agwStyle=FS.FS_LEFT,name=param)
                     if maxLim>1:
-                        paramSlider=wx.Slider(self.commandsPanel,-1,value=midValue,minValue=minLim,maxValue=maxLim,size=(200,20),style=wx.SL_HORIZONTAL|wx.SL_MIN_MAX_LABELS,name=param)
+                        paramFloatSpin.SetFormat("%f")
+                        paramFloatSpin.SetDigits(2)
                     # It's a decimal!:
                     else:
-                        # paramSlider.SetTickFreq((minLim-maxLim)/10.)
-                        pass
-                    # 
-                    hbox.Add(paramText)
-                    hbox.Add(paramSlider)
+                        paramFloatSpin.SetFormat("%f")
+                        paramFloatSpin.SetDigits(2)
+                        paramFloatSpin.SetIncrement(0.01)
+                    hbox.Add(paramText,-1,wx.ALL,5)
+                    hbox.Add(paramFloatSpin,-1,wx.ALL,5)
+            #TLM condition
+            if (i["id"]>=self.TLM_OFFSET): #isTLM
+                self.Bind(wx.EVT_BUTTON,self.buttonPressedTM,button)
+            else:
+                self.Bind(wx.EVT_BUTTON,self.buttonPressed,button)
+            self.vbox.Add(hbox,1,wx.ALL,5)
 
+    def buttonPressed(self,event):
+        commandLine=''
+        commandLine+=self.config["telecommand"][event.GetId()]["code"]
+        commandLine+=' '
+        params=self.config["telecommand"][event.GetId()]["params"]
+        if params==0:
+            pass
+        else:
+            for idx,param in enumerate(params.split(',')):
+                commandLine+= str(self.FindWindowById(100*event.GetId()+idx).GetValue())
+                commandLine+=' '
 
-            self.vbox.Add(hbox,1,wx.ALL,0)
+        self.sendCommand(commandLine)
+
+    def buttonPressedTM(self,event):
+        commandLine=''
+        commandLine+=self.config["telemanipulation"][event.GetId()-self.TLM_OFFSET]["code"]
+        commandLine+=' '
+        params=self.config["telemanipulation"][event.GetId()-self.TLM_OFFSET]["params"]
+        if params==0:
+            pass
+        else:
+            for idx,param in enumerate(params.split(',')):
+                commandLine+= str(self.FindWindowById(100*event.GetId()+idx).GetValue())
+                commandLine+=' '
+
+        self.sendCommandTLM(commandLine)
 
 
 class Terminal(scrolled.ScrolledPanel):
@@ -105,12 +151,20 @@ class Terminal(scrolled.ScrolledPanel):
         text  = "Welcome to pyGCS"
 
         self.terminalBox = wx.TextCtrl(self,0,text,style=wx.TE_MULTILINE)
-        self.terminalBox.SetBackgroundColour((randrange(255),randrange(255),randrange(255)))
-        # self.SetBackgroundColour((255,255,0))
+
+        colour=(randrange(255),randrange(255),randrange(255))
+        colourHSV=colorsys.rgb_to_hsv(colour[0]/255.,colour[1]/255.,colour[2]/255.)
+        
+        inverseColourHSV= tuple((1-colourHSV[0],colourHSV[1],colourHSV[2]))        
+        inverseColour= colorsys.hsv_to_rgb(inverseColourHSV[0],inverseColourHSV[1],inverseColourHSV[2])
+        inverseColour=(inverseColour[0]*255,inverseColour[1]*255,inverseColour[2]*255)
+
+        self.terminalBox.SetBackgroundColour(colour)
+        self.terminalBox.SetForegroundColour(inverseColour)
+
         hbox.Add(self.terminalBox,1,wx.EXPAND|wx.TOP,5)
         self.SetSizer(hbox)
-        # vbox.Add(hbox,1,wx.EXPAND,0)
-        # self.SetSizer(vbox)
+
         self.SetAutoLayout(1) 
         self.SetupScrolling()
 
